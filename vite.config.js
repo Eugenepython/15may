5e55dd8be22cf6eb6d5cac4e70fdd08be02038ed
Eugenepython <50318257+Eugenepython@users.noreply.github.com>
2023-05-16 14:29:18 +0100

    second base path for GitHub pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build',
-    assetsDir: './',
-  },
-  server: {
-    hmr: {
-      protocol: 'ws', 
-    },
-  },
-  base: '/15may/' 
+    base: '/15may' 
 });
 
 
